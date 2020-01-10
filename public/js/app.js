1f75534c7015cf110b35daf96eef6b7a05b38ff0
Faraz Khoshbakhtian <faraz.khoshbakhtian@mail.utoronto.ca>
2020-01-10 18:32:29 -0500

    added bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const location_input = document.getElementById('location')
 const error_p = document.querySelector('#error')
 const data_p = document.querySelector('#data')
 
-m1.textContent = 'Please Enter a location...'
+m1.textContent = 'Your Move!!!'
 weatherForm.addEventListener('submit', (e) => {
     //prevent the browser to refresh by default on submit
     e.preventDefault()
@@ -18,9 +18,14 @@ weatherForm.addEventListener('submit', (e) => {
     fetch(`/wheather?address=${encodeURIComponent(location)}`).then(response => {
         response.json().then(data => {
             if (data.error) {
-                m1.textContent = data.error
+                m1.textContent = ":("
+                m2.textContent = data.error
+                m3.textContent = '';
+                m4.textContent = '';
+                m5.textContent = '';
+
             } else {
-                m1.textContent = `We present to you the infor about ${data.location}:`
+                m1.textContent = `We present to you the info about ${data.location}:`
                 m2.textContent = `Current tempeture is: ${data.current_temp}
                 but it feels like ${data.apparent_temeture}`;
                 m3.textContent = `Summary: ${data.summary}`;
