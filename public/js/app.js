d95ad2bf5c4d38e8671e1a7446ec7ae08e790c37
Faraz Khoshbakhtian <faraz.khoshbakhtian@mail.utoronto.ca>
2020-01-16 23:57:35 -0500

    x overflow problem fixed

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const data_p = document.querySelector('#data')
 
 // set the dimensions and margins of the graph
 var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 920 - margin.left - margin.right,
+    width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
